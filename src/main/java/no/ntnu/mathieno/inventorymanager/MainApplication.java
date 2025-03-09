7069d8cceb9cd65336f15d64fb924fabc877fec7
Mathias Erik Nord <mathias.erik.nord@gmail.com>
2025-03-09 14:15:34 +0100

    Implemented start of a GUI.

--- src/main/java/no/ntnu/mathieno/inventorymanager/MainApplication.java
+++ src/main/java/no/ntnu/mathieno/inventorymanager/MainApplication.java
@@ -1,11 +1,21 @@
 package no.ntnu.mathieno.inventorymanager;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApplication extends javafx.application.Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/no/ntnu/mathieno/inventorymanager/product-overview.fxml"));
+    Scene scene = new Scene(loader.load());
+    primaryStage.setScene(scene);
+    primaryStage.setTitle("Inventory Manager");
+    primaryStage.show();
+  }
 
+  public static void main(String[] args) {
+    launch(args);
   }
 }
---
+++ src/main/java/no/ntnu/mathieno/inventorymanager/backend/controllers/ProductOverviewController.java
@@ -0,0 +1,16 @@
+package no.ntnu.mathieno.inventorymanager.backend.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ProductOverviewController {
+  @FXML
+  private Button addButton;
+  @FXML
+  private Button removeButton;
+
+  @FXML
+  private void addButtonHandler() {
+
+  }
+}
