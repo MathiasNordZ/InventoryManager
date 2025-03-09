a7d6ba9d1156b728e23d6671a5a2eed2c2b7aeea
Mathias Erik Nord <mathias.erik.nord@gmail.com>
2025-03-09 21:50:32 +0100

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,17 @@
 module no.ntnu.mathieno.inventorymanager {
-  requires javafx.controls;
-  requires javafx.fxml;
-  requires com.google.gson;
-  requires java.logging;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires com.google.gson;
+    requires java.logging;
 
-  opens no.ntnu.mathieno.inventorymanager to javafx.fxml, com.google.gson;
-  exports no.ntnu.mathieno.inventorymanager;
-  exports no.ntnu.mathieno.inventorymanager.backend.exceptions;
-  opens no.ntnu.mathieno.inventorymanager.backend.exceptions to com.google.gson, javafx.fxml;
-  exports no.ntnu.mathieno.inventorymanager.backend.models;
-  opens no.ntnu.mathieno.inventorymanager.backend.models to com.google.gson, javafx.fxml;
-  exports no.ntnu.mathieno.inventorymanager.backend.filehandling;
-  opens no.ntnu.mathieno.inventorymanager.backend.filehandling to com.google.gson, javafx.fxml;
-}
+    opens no.ntnu.mathieno.inventorymanager to javafx.fxml, com.google.gson;
+    exports no.ntnu.mathieno.inventorymanager;
+    exports no.ntnu.mathieno.inventorymanager.backend.exceptions;
+    exports no.ntnu.mathieno.inventorymanager.backend.controllers to javafx.fxml;
+    opens no.ntnu.mathieno.inventorymanager.backend.controllers to javafx.fxml;
+    opens no.ntnu.mathieno.inventorymanager.backend.exceptions to com.google.gson, javafx.fxml;
+    exports no.ntnu.mathieno.inventorymanager.backend.models;
+    opens no.ntnu.mathieno.inventorymanager.backend.models to com.google.gson, javafx.fxml;
+    exports no.ntnu.mathieno.inventorymanager.backend.filehandling;
+    opens no.ntnu.mathieno.inventorymanager.backend.filehandling to com.google.gson, javafx.fxml;
+  }
--- src/main/java/no/ntnu/mathieno/inventorymanager/backend/controllers/ProductOverviewController.java
+++ src/main/java/no/ntnu/mathieno/inventorymanager/backend/controllers/ProductOverviewController.java
@@ -2,15 +2,42 @@ package no.ntnu.mathieno.inventorymanager.backend.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
+import no.ntnu.mathieno.inventorymanager.backend.filehandling.JsonHandler;
+import no.ntnu.mathieno.inventorymanager.backend.models.Product;
 
 public class ProductOverviewController {
   @FXML
   private Button addButton;
   @FXML
   private Button removeButton;
+  @FXML
+  private TextField nameField;
+  @FXML
+  private TextField categoryField;
+  @FXML
+  private Spinner<Integer> quantityField;
+
+  private JsonHandler jsonHandler;
+
+  public ProductOverviewController() {
+    jsonHandler = new JsonHandler();
+  }
 
   @FXML
   private void addButtonHandler() {
+    addButton.setOnAction(event -> {
+      Product product = new Product(nameField.getText(), categoryField.getText(), quantityField.getValue());
 
+      jsonHandler.addProduct(product);
+    });
+  }
+
+  @FXML
+  private void init() {
+    SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, 1);
+    quantityField.setValueFactory(valueFactory);
   }
 }
